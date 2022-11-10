f3e038e4c3eab5164dc914194b121b0132b61e8d
sirinapha13 <116251394+sirinapha13@users.noreply.github.com>
2022-11-10 13:59:03 +0700

    Create TestJS.js

---
+++ week4/TestJS.js
@@ -0,0 +1,12 @@
+console.log(5);
+var myname = 'sirinapha';
+let letname = 'madyukop';
+
+const year = 2020;
+//year = 2010;
+
+let grade = 3.56;
+grade = Math.floor(grade);
+console.log(grade);
+
+console.log(`My name is ${myname}`);
