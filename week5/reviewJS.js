8df5d98075d91a7adbd6952f6b72c46e821ee74e
sirinapha13 <116251394+sirinapha13@users.noreply.github.com>
2022-11-18 10:44:14 +0700

    Create reviewJS.js

---
+++ week5/reviewJS.js
@@ -0,0 +1,10 @@
+x=10;
+var y=15;
+let myName = 'Jirawat';
+const year=2023;
+//year=2023;
+console.log("year เป็นชนิด",typeof(year));
+console.log("myName เป็นชนิด",typeof(myName));
+
+y="TNK";
+console.log(y,typeof(y));
