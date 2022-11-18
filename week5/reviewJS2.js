55fcf95f025ea5e7c39c76f376a84a9c412e31dc
sirinapha13 <116251394+sirinapha13@users.noreply.github.com>
2022-11-18 10:44:17 +0700

    Create reviewJS2.js

---
+++ week5/reviewJS2.js
@@ -0,0 +1,9 @@
+let arr = ['JavaScript', 2565, 3.65, true]
+
+console.log(typeof (arr));
+console.log(arr[0]);
+console.log(arr.length);
+
+for (i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
